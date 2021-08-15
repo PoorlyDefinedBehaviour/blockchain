de1f8b2df0353c99b5e5d8e348096dc4ff19d4ce
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-15 16:13:44 -0300

    feat(consensus): resolve conflicts

--- src/consensus.rs
+++ src/consensus.rs
@@ -1,8 +1,10 @@
 use crate::chain::Chain;
 use crate::proof_of_work;
+use crate::viewmodel;
+use reqwest;
 
-pub fn is_chain_valid(chain: &Chain) -> bool {
-  let mut previous_block = &chain.blocks[0];
+fn is_chain_valid(chain: &Chain) -> bool {
+  let mut previous_block = chain.blocks.first().unwrap();
 
   for block in chain.blocks.iter().skip(1) {
     if previous_block.hash() != block.previous_block_hash {
@@ -23,4 +25,26 @@ pub fn is_chain_valid(chain: &Chain) -> bool {
   true
 }
 
-pub fn resolve_conflicts(chain: &Chain) {}
+pub async fn resolve_conflicts(chain: &Chain) -> Result<Chain, Box<dyn std::error::Error>> {
+  let mut longest_chain = chain.clone();
+
+  // TODO: make requests parallel
+  for node in &chain.nodes {
+    let url = format!("{}/chain", node.to_string());
+
+    let chain_viewmodel = reqwest::get(url)
+      .await?
+      .json::<viewmodel::ChainViewModel>()
+      .await?;
+
+    let other_chain = Chain::from(chain_viewmodel);
+
+    if other_chain.blocks.len() > chain.blocks.len() && is_chain_valid(&other_chain) {
+      longest_chain = other_chain;
+    }
+  }
+
+  dbg!(&longest_chain);
+
+  Ok(longest_chain)
+}
