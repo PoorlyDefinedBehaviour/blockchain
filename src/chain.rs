f36e080a35f318c5304d65d879bcdb00d459ddd0
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-15 01:36:42 -0300

    fix(chain): block timestamp

--- src/chain.rs
+++ src/chain.rs
@@ -2,42 +2,53 @@ use crate::block::{Block, Transaction};
 
 #[derive(Debug)]
 pub struct Chain {
-  blocks: Vec<Block>,
-  transactions: Vec<Transaction>,
+  pub blocks: Vec<Block>,
+  pub transactions: Vec<Transaction>,
 }
 
 impl Chain {
   pub fn new() -> Self {
-    let mut chain = Chain {
-      blocks: Vec::new(),
+    Chain {
+      blocks: vec![Block {
+        index: 0,
+        timestamp: std::time::SystemTime::now()
+          .duration_since(std::time::SystemTime::UNIX_EPOCH)
+          .unwrap()
+          .as_millis(),
+        transactions: Vec::new(),
+        proof: 100,
+        previous_block_hash: "".to_owned(),
+      }],
       transactions: Vec::new(),
-    };
-
-    chain.block(100, "".to_owned());
-
-    chain
+    }
   }
-  pub fn transaction(&mut self, sender: String, recipient: String, amount: i64) -> usize {
-    self.transactions.push(Transaction {
-      sender,
-      recipient,
-      amount,
-    });
+
+  pub fn transaction(&mut self, transaction: Transaction) -> usize {
+    self.transactions.push(transaction);
 
     let last_block = self.blocks.last().unwrap();
 
     last_block.index + 1
   }
 
-  fn block(&mut self, proof: usize, previous_block_hash: String) {
-    self.blocks.push(Block {
-      index: self.blocks.len() + 1,
-      timestamp: 1,
+  pub fn block(&mut self, proof: usize) -> Block {
+    let previous_block_hash = self.blocks.last().unwrap().hash();
+
+    let block = Block {
+      index: self.blocks.len(),
+      timestamp: std::time::SystemTime::now()
+        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis(),
       transactions: self.transactions.clone(),
       proof,
       previous_block_hash,
-    });
+    };
+
+    self.blocks.push(block.clone());
 
     self.transactions.clear();
+
+    block
   }
 }
