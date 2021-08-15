f8491f0632f62d99a17ceb013c10d0b12f00de4e
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-15 01:37:06 -0300

    refactor(block): make Transaction.sender a sum type

--- src/block.rs
+++ src/block.rs
@@ -1,23 +1,30 @@
+use serde::{Deserialize, Serialize};
 use sha2::Digest;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum Sender {
+  System,
+  Client(String),
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Transaction {
-  pub sender: String,
+  pub sender: Sender,
   pub recipient: String,
   pub amount: i64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Block {
   pub index: usize,
-  pub timestamp: usize,
+  pub timestamp: u128,
   pub transactions: Vec<Transaction>,
   pub previous_block_hash: String,
   pub proof: usize,
 }
 
 impl Block {
-  fn hash(&self) -> String {
+  pub fn hash(&self) -> String {
     let block_as_string = format!("{:?}", self);
 
     format!("{:x}", sha2::Sha256::digest(block_as_string.as_bytes()))
